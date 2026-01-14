b56f981e20962aef046e2566e9acecb74b50b431
Rishab Raj <rr0948555@gmail.com>
2026-01-14 13:28:49 +0530

    useLocation Hook

--- src/MyRouters/About.jsx
+++ src/MyRouters/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <>
      <h2>This is about us Page</h2> 
-     <li><Link to="/employee/alex/23">Alex</Link></li>
+     <li><Link to="/employee/alex/23" state={{name:'Alex Pandian', age:23}}>Alex</Link></li>
      <li><Link to="/employee/tovino/22">Tovino</Link></li>
     </>
   )
--- src/MyRouters/Home.jsx
+++ src/MyRouters/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
      <h2>This is Home page</h2> 
