35a82873221287b25adfb36ac75bd896559aa136
Rishab Raj <rr0948555@gmail.com>
2026-01-14 13:19:40 +0530

    UseNavigate Hook

--- src/MyRouters/Contact.jsx
+++ src/MyRouters/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+      let bool = true;
+      // let bool = false; // navigate to about page
+      if(bool){
+        navigate('/')
+      }
+      else {
+        navigate('/about')
+      }
+  }
   return (
     <>
       <h2>Contact</h2>
+      {/* <button onClick={() => navigate('/')}>Go to Home Page</button> */}
+      <button onClick={() => handleClick()}>Go to Home Page</button>
+      {/* <button onClick={() => navigate('/about')}>Go To About Page</button> */}
+      <button onClick={() => handleClick()}>Go To About Page</button>
     </>
   )
 }
